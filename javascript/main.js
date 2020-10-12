6cf8b9fdc742979cf119a3cc6f91370f4ba4c35e
Vivian Dang <dangvv@dukes.jmu.edu>
2020-10-12 12:59:04 -0400

    adapted for dropdown menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,20 @@
 const navg = document.getElementById("navigation");
 const menuButton = document.getElementById("menu-button");
 
+const portfolioButton = document.getElementById("dropdown");
+const dropdown = document.getElementById("dropdown-content");
+
 menuButton.addEventListener("click", event => {
     navg.classList.toggle("open");
     event.stopPropagation();
 });
 
+portfolioButton.addEventListener("click", event => {
+    dropdown.classList.toggle("expand");
+    event.stopPropagation();
+});
+
 document.addEventListener("click", () => {
     navg.classList.remove("open");
+    dropdown.classList.remove("expand");
 });
